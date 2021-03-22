869dfa7c8d9c0a96b7658b95ab2201303adf4f61
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-22 21:54:52 +0100

    use bootstrap layout with col-6 and justify-content-center

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Aside from './components/Aside';
 import Article from './components/Article';
 
 function App() {
 	return (
 		<main>
-			<Article/>
-			<Aside/>
+			<div className="flex">
+				<div className="flex-75-percent">
+					<Article/>
+
+				</div>
+				<div className="flex-25-percent flex">
+					<Aside/>
+				</div>
+			</div>
 		</main>
 	)
 }
--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -7,29 +7,66 @@ function Article() {
 		<article>
 			<Header/>
 
-			<h2>Let's set up your account</h2>
-			<p>Already have an account?&nbsp;
-				<a href="">Sign in</a>
-			</p>
-			<input placeholder="Your name"/>
-			<input placeholder="Email address"/>
-			<select>
-				<option value="" disabled selected>I would describe emy user type as</option>
-				<option value="Developer">Developer</option>
-				<option value="Designer">Designer</option>
-			</select>
-			<input placeholder="Password" type="password"/>
-			<small>Minimum 8 characters</small>
-			<button>Next</button>
-			<p>By clicking the "Next" button, you agree to creating a free account, and to &nbsp;
-				<a href="">
-					Terms of Service
-				</a>
-				&nbsp; and &nbsp;
-				<a href="">
-					Privacy Policy
-				</a>
-				.</p>
+			<div className="container-fluid">
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<h2>Let's set up your account</h2>
+
+						<p>Already have an account?&nbsp;
+							<a href="">Sign in</a>
+						</p>
+
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<input placeholder="Your name"/>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<input placeholder="Email address"/>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<select>
+							<option value="" disabled selected>I would describe emy user type as</option>
+							<option value="Developer">Developer</option>
+							<option value="Designer">Designer</option>
+						</select>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<input placeholder="Password" type="password"/>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<small>Minimum 8 characters</small>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<button className="margin-top-20px width-100-percent">Next</button>
+					</div>
+				</div>
+				<div className="row justify-content-center">
+					<div className="col-6">
+						<p className="margin-top-20px">By clicking the "Next" button, you agree to creating a free account, and to &nbsp;
+							<a href="">
+								Terms of Service
+							</a>
+							&nbsp; and &nbsp;
+							<a href="">
+								Privacy Policy
+							</a>
+							.
+						</p>
+					</div>
+				</div>
+			</div>
 		</article>
 	)
 }
