6f940b941de95c5e6327d958a72ab85f23289b44
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-22 20:31:42 +0100

    * change styles for step 1 of 3

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import './Article.css'
 
 function Article() {
 	return (
 		<article>
-			<header> Step 1 of 3</header>
+			<header className="text-align-right padding-20-px">
+				<p className="font-weight-bolder">
+					Step 1 of 3
+					<span className="dot background-color-black"></span>
+					<span className="dot"></span>
+					<span className="dot"></span>
+				</p>
+			</header>
 			<h2>Let's set up your account</h2>
 			<p>Already have an account?&nbsp;
 				<a href="">Sign in</a>
