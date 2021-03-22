9bd2df629c417254e0bec0d60a12fb16d18bc414
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-22 23:12:24 +0100

    add password toggle

--- src/components/Password.tsx
+++ src/components/Password.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState} from "react";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faEye} from "@fortawesome/free-solid-svg-icons";
 import './Password.css'
 
+const eye = <FontAwesomeIcon icon={faEye}/>;
+
 function Password() {
+	const [passwordShown, setPasswordShown] = useState(false);
+	const toggleVisibility = () => {
+		setPasswordShown(passwordShown ? false : true);
+	};
+
 	return (
 		<div className="row justify-content-center">
 			<div className="col-6">
-				<input placeholder="Password" type="password" className="margin-bottom-5px"/>
+				<div className="password-wrapper">
+					{" "}
+					<input
+						placeholder="Password"
+						name="password"
+						className="margin-bottom-5px"
+						type={passwordShown ? "text" : "password"}
+					/>
+					<i onClick={toggleVisibility}>{eye}</i>{" "}
+				</div>
 				<small className="gray">Minimum 8 characters</small>
 			</div>
 		</div>
