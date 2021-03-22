e8fda2897a9b54664796f1be5ae50404a38ffd89
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-23 00:35:40 +0100

    * show fieldset on focus

--- src/components/input.tsx
+++ src/components/input.tsx
@@ -1,15 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Input.css'
 
 function Input() {
+	const [focus, onFocus] = useState(false);
+	const toggleFocus = () => {
+		onFocus(focus ? false : true);
+	};
+
 	return (
 		<div className="row justify-content-center">
 			<div className="col-6">
-				<fieldset>
-					<legend>Email address</legend>
-					{/*<input placeholder="Email address"/>*/}
-					<input/>
-				</fieldset>
+				{focus
+					? <fieldset>
+						<legend>Email address</legend>
+						<input autoFocus/>
+					</fieldset>
+					: <input placeholder="Email address" onFocus={toggleFocus}/>
+				}
 			</div>
 		</div>
 	)
