1610400c25dc68257a9eb4ccf5bc6d32a880a676
extern-ge-yuqiu <extern.ge_yuqiu@allianz.de>
2021-03-23 01:35:48 +0100

    * add onChange for select

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -17,6 +17,8 @@ function Article() {
 		emailAddressChange(event.target.value);
 	};
 
+	const [role, roleChange] = useState("");
+
 	const [password, passwordChange] = useState("");
 	const onPasswordChange = (event: any) => {
 		passwordChange(event.target.value);
@@ -45,7 +47,7 @@ function Article() {
 				/>
 				<div className="row justify-content-center">
 					<div className="col-6">
-						<select required>
+						<select required value={role} onChange={e => roleChange(e.target.value)}>
 							<option value="" disabled selected>I would describe emy user type as</option>
 							<option value="Developer">Developer</option>
 							<option value="Designer">Designer</option>
